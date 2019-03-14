650751a4ac1c7fc5fc7fcc2bdeec3d6e9fed790c
Eligijus Stugys <eligijus.stugys@real-digital.de>
2019-03-14 10:05:49 +0200

    Implementing training.

--- html/number_recognize/Mnist.php
+++ html/number_recognize/Mnist.php
@@ -2,12 +2,21 @@
 
 declare(strict_types=1);
 
+// Disable the memory limit to avoid "Allowed memory size exhausted" errors:
+//ini_set('memory_limit', '-1');
+
+// Set memory limit to 3 Gb (Yes, it eating a lot memory...):
+ini_set('memory_limit', '3G');
+
+
 use number_recognize\MnistDataSetReader;
 use number_recognize\MnistDataSetReaderTesting;
 
 require_once 'MnistDataSetReaderTesting.php';
 require_once 'MnistDataSetReader.php';
+require_once 'MnistDataset.php';
 
+// Attempt to clear console:
 //system('cls');
 //system('clear');
 
@@ -19,12 +28,12 @@ $BATCH_SIZE = 100;
 $STEPS = 1000;
 
 // Load Training Dataset
-$trainImagePath = 'data/mnist/train-images-idx3-ubyte';
-$trainLabelPath = 'data/mnist/train-labels-idx1-ubyte';
+$trainImagePath = 'data/mnist/train-images.idx3-ubyte';
+$trainLabelPath = 'data/mnist/train-labels.idx1-ubyte';
 
 // Check specified arguments:
 if (empty($argv[1])) {
-	echo "\nERROR: Please specify action.\n";
+    echo "\nERROR: Please specify action.\n";
     return;
 }
 
@@ -33,7 +42,34 @@ switch ($argv[1]) {
     case 'generate_images':
         MnistDataSetReaderTesting::generateImages('data/mnist/t10k-images.idx3-ubyte');
         break;
+    case 'train_network':
+
+        // ---------------------- BEGIN -----------------------------------
+
+        // Define application start time:
+        $milliseconds = round(microtime(true) * 1000);
+
+        // Print message, that starting loading:
+        echo date_format(new \DateTime(),
+                'Y.m.d H:i:s') . 'INFO: Loading training dataset... (may take a while)' . PHP_EOL;
+
+        // Loading training data:
+        $trainDataset = MnistDataSetReader::fromFiles($trainImagePath, $trainLabelPath);
+
+        // Begin Training
+        $batches = $trainDataset->getSize() / $BATCH_SIZE;
+
+        // Information about results:
+        echo date_format(new \DateTime(),
+                'Y.m.d H:i:s') . " INFO: Done training in " . (round(microtime(true) * 1000) - $milliseconds) . " milliseconds\n";
+        echo date_format(new \DateTime(), 'Y.m.d H:i:s') . " INFO: Memory used: " . memory_get_usage(true) . " bytes\n";
+        echo date_format(new \DateTime(),
+                'Y.m.d H:i:s') . " INFO: peak of memory allocated by PHP: " . memory_get_peak_usage(true) . " bytes\n";
+
+        // ---------------------- END -----------------------------------
+
+        break;
     default:
-		echo "\nERROR: Unhandled action '" . $argv[1] . "'.\n";
-		return;
+        echo "\nERROR: Unhandled action '" . $argv[1] . "'.\n";
+        return;
 }
---
+++ html/number_recognize/NeuralNetworkTraining.php
@@ -0,0 +1,138 @@
+<?php
+
+declare(strict_types=1);
+
+namespace number_recognize;
+
+class NeuralNetworkTraining
+{
+    // This will be a one dimensional array (vector) [10]:
+    private $b;
+
+    // This will be a two dimensional array (matrix) [784x10]:
+    private $W;
+
+    /**
+     * Initialise the bias vector and weights as random values between 0 and 1.
+     */
+    public function __construct()
+    {
+        $this->b = [];
+        $this->W = [];
+        for ($i = 0; $i < MnistDataset::LABELS; $i++) {
+            $this->b[$i] = random_int(1, 1000) / 1000;
+            $this->W[$i] = [];
+            for ($j = 0; $j < MnistDataset::IMAGE_SIZE; $j++) {
+                $this->W[$i][$j] = random_int(1, 1000) / 1000;
+            }
+        }
+    }
+
+    /**
+     * The softmax layer maps an array of activations to a probability vector.
+     */
+    private function softmax(array $activations): array
+    {
+        // Normalising with the max activation makes the computation more numerically stable
+        $max = max($activations);
+        $activations = array_map(function ($a) use ($max) {
+            return exp($a - $max);
+        }, $activations);
+        $sum = array_sum($activations);
+        return array_map(function ($a) use ($sum) {
+            return $a / $sum;
+        }, $activations);
+    }
+
+    /**
+     * Forward propagate through the neural network to calculate the activation
+     * vector for an image.
+     */
+    public function hypothesis(array $image): array
+    {
+        $activations = [];
+        // Computes: Wx + b
+        for ($i = 0; $i < MnistDataset::LABELS; $i++) {
+            $activations[$i] = $this->b[$i];
+            for ($j = 0; $j < MnistDataset::IMAGE_SIZE; $j++) {
+                $activations[$i] += $this->W[$i][$j] * $image[$j];
+            }
+        }
+        return $this->softmax($activations);
+    }
+
+    /**
+     * Calculate the gradient adjustments on a single training example (image)
+     * from the dataset.
+     *
+     * Returns the contribution to the loss value from this example.
+     */
+    private function gradientUpdate(array $image, array &$bGrad, array &$WGrad, int $label): float
+    {
+        $activations = $this->hypothesis($image);
+        for ($i = 0; $i < MnistDataset::LABELS; $i++) {
+            // Uses the derivative of the softmax function
+            $bGradPart = ($i === $label) ? $activations[$i] - 1 : $activations[$i];
+            for ($j = 0; $j < MnistDataset::IMAGE_SIZE; $j++) {
+                // Gradient is the product of the bias gradient and the input activation
+                $WGrad[$i][$j] += $bGradPart * $image[$j];
+            }
+            $bGrad[$i] += $bGradPart;
+        }
+        // Cross entropy
+        return 0 - log($activations[$label]);
+    }
+
+    /**
+     * Perform one step of gradient descent on the neural network using the
+     * provided dataset.
+     *
+     * Returns the total loss for the network on the provided dataset.
+     */
+    public function trainingStep(MnistDataset $dataset, float $learningRate): float
+    {
+        // Zero init the gradients
+        $bGrad = array_fill(0, MnistDataset::LABELS, 0);
+        $WGrad = array_fill(0, MnistDataset::LABELS, array_fill(0, MnistDataset::IMAGE_SIZE, 0));
+        $totalLoss = 0;
+        $size = $dataset->getSize();
+        // Calculate the gradients and loss
+        for ($i = 0; $i < $size; $i++) {
+            $totalLoss += $this->gradientUpdate($dataset->getImage($i), $bGrad, $WGrad, $dataset->getLabel($i));
+        }
+        // Adjust the weights and bias vector using the gradient and the learning rate
+        for ($i = 0; $i < MnistDataset::LABELS; $i++) {
+            $this->b[$i] -= $learningRate * $bGrad[$i] / $size;
+            for ($j = 0; $j < MnistDataset::IMAGE_SIZE; $j++) {
+                $this->W[$i][$j] -= $learningRate * $WGrad[$i][$j] / $size;
+            }
+        }
+        return $totalLoss;
+    }
+
+    function sigmoid($t)
+    {
+        return 1 / (1 + exp(-$t));
+    }
+
+    /**
+     * Softmax calculation for PHP (useful for logistic classifications)
+     *
+     * @param array $v
+     *
+     * @return array
+     */
+    function softmax2(array $v)
+    {
+
+        //Just in case values are passed in as string, apply floatval
+        $v = array_map('exp', array_map('floatval', $v));
+        $sum = array_sum($v);
+
+        foreach ($v as $index => $value) {
+            $v[$index] = $value / $sum;
+        }
+
+        return $v;
+    }
+}
