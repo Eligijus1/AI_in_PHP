3ca8454dd5202ca1f2f453def2502e99e3cb85e4
Eligijus Stugys <eligijus.stugys@real-digital.de>
2020-12-05 12:22:13 +0200

    Tiuning calculation.

--- html/number_recognize/helpers/SigmoidTestHelper.php
+++ html/number_recognize/helpers/SigmoidTestHelper.php
@@ -65,11 +65,11 @@ class SigmoidTestHelper
         HelperFunctions::printInfo("Done testing in " . HelperFunctions::formatMilliseconds(round(microtime(true) * 1000) - $milliseconds));
         HelperFunctions::printInfo("Data location: " . self::DATA_LOCATION);
         HelperFunctions::printInfo("Used for testing {$imagesCount} images and {$labelsCount} labels.");
-        HelperFunctions::printInfo("Success guess amount: {$successGuessAmount}.");
         HelperFunctions::printInfo("Learning rate: {$sigmoid->getLearningRate()}.");
         HelperFunctions::printInfo("Momentum: {$sigmoid->getMomentum()}.");
         HelperFunctions::printInfo("Epochs number: {$sigmoid->getMaxNumEpochs()}.");
         HelperFunctions::printInfo("Global error: {$sigmoid->getLatestTrainGlobalError()}.");
+        HelperFunctions::printInfo("Success guess amount: {$successGuessAmount} from {$imagesCount}.");
 
         return $successGuessAmount;
     }
